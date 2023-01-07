4f37b59f946aa07af4ac6e34fc43e4930eae01bd
dandi-apriadi <60336465+dandi-apriadi@users.noreply.github.com>
2023-01-08 00:33:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
-set -eu 
+set -eu
 
 export PYTHONUNBUFFERED=true
 
 VIRTUALENV=.data/venv
 
-if [! -d $VIRTUALENV]; then
-    python3 -m venv $VIRTUALENV
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
 fi
 
-if [! -d $VIRTUALENV/bin/pip];then
-    curl --silent --show-error 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python3
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
